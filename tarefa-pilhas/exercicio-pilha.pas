49f043369374b33a68bc9160e2de87d5f8014da6
unknown <jp.ragdool@hotmail.com>
2023-09-05 19:59:11 -0300

    fix pascal

--- tarefa-pilhas/exercicio-pilha.pas
+++ tarefa-pilhas/exercicio-pilha.pas
@@ -1,5 +1,9 @@
-program PilhaReversa;
+////////////////////////////////////////////
+// JO√O PAULO DA ROSA E NAT√ DOS SANTOS ////
+////////////////////////////////////////////
 
+program PilhaPalabra;
+Uses Crt;
 const
   MAX_TAMANHO = 25;
 
@@ -10,50 +14,53 @@ type
     conteudo: array[1..MAX_TAMANHO] of NODO;
   end;
 
-procedure criaPilha(var pilha: PILHA);
-begin
-  pilha.topo := 0;
-end;
-
-function pop(var pilha: PILHA): NODO;
-begin
-  pop := pilha.conteudo[pilha.topo];
-  pilha.topo := pilha.topo - 1;
-end;
-
-procedure push(var pilha: PILHA; caracter: NODO);
-begin
-  pilha.topo := pilha.topo + 1;
-  pilha.conteudo[pilha.topo] := caracter;
-end;
-
-function isVazio(var pilha: PILHA): Boolean;
-begin
-  isVazio := pilha.topo = 0;
-end;
-
 var
-  pilha: PILHA;
-  palavra: array[1..MAX_TAMANHO] of Char;
+  p: PILHA;
+  palavra: string;
   i, tamanho_palavra: Integer;
 
+procedure criaPilha(var arg_pilha: PILHA);
 begin
-  criaPilha(pilha);
+  arg_pilha.topo := 0;
+end;
+
+function pop(var arg_pilha: PILHA): NODO;
+begin
+  pop := arg_pilha.conteudo[arg_pilha.topo];
+  arg_pilha.topo := arg_pilha.topo - 1;
+end;
+
+procedure push(var arg_pilha: PILHA; caracter: NODO);
+begin
+  arg_pilha.topo := arg_pilha.topo + 1;
+  arg_pilha.conteudo[arg_pilha.topo] := caracter;
+end;
+
+function isVazio(var arg_pilha: PILHA): Boolean;
+begin
+  isVazio := arg_pilha.topo = 0;
+end;
+
+
+begin
+  criaPilha(p);
 
   Write('Informe uma palavra: ');
   ReadLn(palavra);
-
+  
   // Enche a pilha a partir de cada caracter
   tamanho_palavra := Length(palavra);
   for i := 1 to tamanho_palavra do
   begin
-    push(pilha, palavra[i]);
+    push(p, palavra[i]);
   end;
 
-  // Enquanto a pilha n√£o estiver vazia realiza o pop
+  // Enquanto a pilha n„o estiver vazia realiza o pop
   Write('Palavra invertida: ');
-  while not isVazio(pilha) do
+  while not isVazio(p) do
   begin
-    Write(pop(pilha));
+    Write(pop(p));
   end;
+  
+  ReadLn();
 end.
