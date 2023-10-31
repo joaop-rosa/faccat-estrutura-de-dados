ed45d3c04472c9de0435e2d252f56824adc71d58
unknown <jp.ragdool@hotmail.com>
2023-10-31 19:43:14 -0300

    lista dinamica encadeada pratica

--- tarefa-fila-12_09/tarefa-fila.pas
+++ tarefa-fila-12_09/tarefa-fila.pas
@@ -74,12 +74,16 @@ begin
 	else
 		begin	
 			if arg_f.tamanho = 1 then
-				arg_f.primeiro := 0
-			else	
-				arg_f.primeiro := arg_f.primeiro + 1; 
+				arg_f.primeiro := 0;
 				
-		  	
   			arg_f.tamanho := arg_f.tamanho - 1;
+
+			if ( arg_f.tamanho >= 1 ) and ( arg_f.primeiro = MAX ) then
+				arg_f.primeiro := 1
+			else
+				arg_f.primeiro := arg_f.primeiro + 1; 
+
+			readln;
 	  	end;
   
 end;                                                
@@ -93,11 +97,13 @@ begin
 		else 
 			begin
 				Writeln('--- Primeiro pedido ---');  
+				Writeln('Indice: ', arg_f.primeiro);
 				Writeln('Numero do pedido: ', arg_f.conteudo[arg_f.primeiro].nro_pedido);
 				Writeln('Cliente: ', arg_f.conteudo[arg_f.primeiro].cliente);
 				Writeln('Quantidade: ', arg_f.conteudo[arg_f.primeiro].qtd);
 				Writeln('-------------------');
 				Writeln('--- Ultimo pedido ---'); 
+				Writeln('Indice: ', arg_f.ultimo);
 				Writeln('Numero do pedido: ', arg_f.conteudo[arg_f.ultimo].nro_pedido);
 				Writeln('Cliente: ', arg_f.conteudo[arg_f.ultimo].cliente);
 				Writeln('Quantidade: ', arg_f.conteudo[arg_f.ultimo].qtd);
--- tarefa-fila/fila_estatica_2023_e.pas
+++ tarefa-fila/fila_estatica_2023_e.pas
@@ -1,94 +1,104 @@
-Program fila_estatica ;
-uses crt;
-const MAX=5; //até 5 elementos
+Program fila_estatica_2023_e ;
+const MAX=5; //atï¿½ 5 elementos
 
-var opcao,valorFila:integer;
-    primeiro:integer; //primeiro a ser atentido
-    ultimo:integer; //ultimo a ser atendido
-    fila: array[1..MAX] of integer;
-    n:integer;//número de integrantes da fila    
+type
+  NODO = integer;
+  FILA = record
+    conteudo: array[1..MAX] of integer;
+    tamanho: integer;
+    primeiro:integer;
+    ultimo:integer;
+  end;
 
-
-
-
-{* Cria a fila *}
-procedure criaFila();
+var f: FILA; 
+		opcao,valorFila,i:integer;
+  
+procedure criaFila(var arg_f: FILA);
 begin
-  primeiro:=0;
-  ultimo:=0;
-  n:=0;
+  arg_f.primeiro:=0;
+  arg_f.ultimo:=0;
+  arg_f.tamanho:=0;
 end;
 
-{* zerar fila *}
-procedure zerarFila();
+procedure zerarFila(var arg_f: FILA);
 begin  
-  criaFila();//cham o metodo novamente
+  criaFila(arg_f);
 end;
 
-(*adiciona um elemento na fila*)
-procedure queue(valor:integer);
+procedure queue( var arg_f: FILA; valor: NODO);
 begin
-  //implementar aqui
+	if arg_f.primeiro = 0 then
+		arg_f.primeiro := 1;
+		arg_f.tamanho := arg_f.tamanho + 1;
+	if arg_f.ultimo = MAX then
+		begin
+		 Write('pilha cheia');
+		 readln();
+		end
+	else
+		begin	
+		  arg_f.ultimo := arg_f.ultimo + 1;
+		  arg_f.tamanho := arg_f.tamanho + 1;
+		  arg_f.conteudo[arg_f.ultimo] := valor;
+	  end;
 end;
 
-(*remove o primeiro elemento da fila*)
-procedure dequeue();
+procedure dequeue(var arg_f: FILA);
 begin
-  //implementar aqui
-   
+	if arg_f.tamanho = 0 then
+		begin
+		 Write('pilha vazia');
+		 readln();
+		end
+	else
+		begin	
+		  arg_f.primeiro := arg_f.primeiro + 1; 
+  		arg_f.tamanho := arg_f.tamanho - 1;
+	  end;
+  
 end;                                                
 
-(*imprime a fila*)
-procedure imprimeFila();
-
+procedure imprimeFila(var arg_f: FILA);	
 begin
-   //implementar aqui 
-   
+		if arg_f.tamanho = 0  then
+			Write('pilha vazia')
+		else 
+	   	for i := arg_f.primeiro to arg_f.ultimo do
+		  begin
+		     Write(arg_f.conteudo[i]);
+		     Write('-');
+		  end;
 end;
 
-
-
-
-
-(*Programa Principal*)
 begin
- //primeiro cria a fila, passado o f com o parâmetro
- criaFila();
- 
+ criaFila(f);
  
  repeat
-      clrscr; //limpa a tela
 		writeln('1-Queue    2-Dequeue  3-Imprimir  4 Zerar Fila 5- SAIR');
 		write('Digite uma opcao: ');
 		readln(opcao);
-		
-		
 	
-		
 		case opcao of
 			1:begin
 			     write('Digite um valor para enfileirar: ');
 			     readln(valorFila);
-			     
-			     queue(valorFila); 
-					       
-			   
+			     queue(f, valorFila); 
         end;
       2:begin
-             dequeue();
+             dequeue(f);
            	 readln;
         
 				end;                           
 		    3:begin
-			      imprimeFila();
+			      imprimeFila(f);
             readln;
 			     end;						
 			4:begin
-			    zerarFila();
+			    zerarFila(f);
           
 			  end;			
 		end;
 		
-    until (opcao=5); //se a opocao=5 sai do programa
+    until (opcao=5);
   
 End.
